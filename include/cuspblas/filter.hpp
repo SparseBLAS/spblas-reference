a91de9428438a8091ad8cbee6ea0b458da392c50
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2024-10-24 17:50:39 +0200

    add/filter

---
+++ include/cuspblas/filter.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cstdint>
+
+namespace spblas {
+struct keep_valid_nonzeros {};
+struct get_lower_matrix {
+  bool include_diag;
+};
+struct get_upper_matrix {
+  bool include_diag;
+};
+
+namespace cuda {
+/**
+ * Count the nnz per row after filter.
+ */
+template <typename ValueType, typename IndexType, typename Filter>
+void csr_filter_count_row_nnz(std::int64_t n, std::int64_t nnz,
+                              const IndexType* row_ptrs,
+                              const IndexType* col_idxs, const ValueType* vals,
+                              Filter filter, IndexType* out_row_ptrs,
+                              std::int64_t* out_nnz);
+/**
+ * fill the column and values after filter
+ */
+template <typename ValueType, typename IndexType, typename Filter>
+void csr_filter_fill(std::int64_t n, std::int64_t nnz,
+                     const IndexType* row_ptrs, const IndexType* col_idxs,
+                     const ValueType* vals, Filter filter, std::int64_t out_nnz,
+                     const IndexType* out_row_ptrs, IndexType* out_col_idxs,
+                     ValueType* out_vals);
+
+} // namespace cuda
+} // namespace spblas
