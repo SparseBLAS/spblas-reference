cabe946ddab0db0c0efc3be51b40e87901a96ab3
Benjamin Brock <benjamin.brock@intel.com>
2025-03-04 15:37:49 -0800

    Add transpose algorithm

--- test/gtest/transpose_test.cpp
+++ test/gtest/transpose_test.cpp
@@ -14,53 +14,75 @@ TEST(CsrView, Transpose) {
   using O = spblas::offset_t;
 
   for (auto&& [m, k, nnz] : util::dims) {
-    SCOPED_TRACE(m);
-    SCOPED_TRACE(k);
-    SCOPED_TRACE(nnz);
+    // Generate CSR Matrix A.
     auto [a_values, a_rowptr, a_colind, a_shape, a_nnz] =
         spblas::generate_csr<T, I>(m, k, nnz);
-    spblas::index<> b_shape{a_shape[1], a_shape[0]};
-    auto b_nnz = a_nnz;
-    std::vector<O> b_rowptr(b_shape[0] + 1);
-    std::vector<I> b_colind(a_nnz);
-    std::vector<T> b_values(a_nnz);
 
-    spblas::csr_view<T, I> a(a_values, a_rowptr, a_colind, a_shape, a_nnz);
-    spblas::csr_view<T, I> b(b_values, b_rowptr, b_colind, b_shape, b_nnz);
+    spblas::csr_view<T, I, O> a(a_values, a_rowptr, a_colind, a_shape, a_nnz);
+
+    // Transpose; B = A_T
+
+    spblas::index b_shape(a.shape()[1], a.shape()[0]);
+
+    std::vector<O> b_rowptr(b_shape[0] + 1);
+    std::vector<I> b_colind(a.size());
+    std::vector<T> b_values(a.size());
+
+    spblas::csr_view<T, I, O> b(b_values, b_rowptr, b_colind, b_shape,
+                                a.size());
 
     auto info = spblas::transpose_inspect(a, b);
-
     spblas::transpose(info, a, b);
 
-    std::vector<I> a_rowind;
-    std::vector<I> b_rowind;
+    // Create transposed COO for reference.
+    std::vector<T> ref_values;
+    std::vector<I> ref_rowind;
+    std::vector<I> ref_colind;
 
     for (auto&& [i, row] : spblas::__backend::rows(a)) {
       for (auto&& [j, v] : row) {
-        a_rowind.push_back(i);
+        ref_values.push_back(v);
+        ref_rowind.push_back(j);
+        ref_colind.push_back(i);
       }
     }
+
+    // Create COO from transposed matrix for test.
+    std::vector<T> test_values;
+    std::vector<T> test_rowind;
+    std::vector<T> test_colind;
+
     for (auto&& [i, row] : spblas::__backend::rows(b)) {
       for (auto&& [j, v] : row) {
-        b_rowind.push_back(i);
+        test_values.push_back(v);
+        test_rowind.push_back(i);
+        test_colind.push_back(j);
       }
     }
+
+    // Ensure both COO matrices are sorted.
     spblas::__ranges::sort(
-        spblas::__ranges::views::zip(a_colind, a_rowind, a_values));
-    // there may be duplicate values in rows, so we need to sort each row of b
-    // by column index
-    for (auto i : spblas::__ranges::views::iota(I{}, b_shape[0])) {
-      const auto b_begin = b_rowptr[i];
-      const auto b_end = b_rowptr[i + 1];
-      const auto row_colind =
-          std::span{b_colind.begin() + b_begin, b_colind.begin() + b_end};
-      const auto row_values =
-          std::span{b_values.begin() + b_begin, b_values.begin() + b_end};
-      spblas::__ranges::sort(
-          spblas::__ranges::views::zip(row_colind, row_values));
+        spblas::__ranges::views::zip(ref_rowind, ref_colind, ref_values));
+    spblas::__ranges::sort(
+        spblas::__ranges::views::zip(test_rowind, test_colind, test_values));
+
+    EXPECT_EQ(ref_values.size(), test_values.size());
+    EXPECT_EQ(ref_rowind.size(), test_rowind.size());
+    EXPECT_EQ(ref_colind.size(), test_colind.size());
+
+    for (auto&& [a, b] :
+         spblas::__ranges::views::zip(ref_values, test_values)) {
+      EXPECT_EQ_(a, b);
     }
-    EXPECT_EQ(a_rowind, b_colind);
-    EXPECT_EQ(a_colind, b_rowind);
-    EXPECT_EQ(a_values, b_values);
+
+    for (auto&& [a, b] :
+         spblas::__ranges::views::zip(ref_rowind, test_rowind)) {
+      EXPECT_EQ(a, b);
+    }
+
+    for (auto&& [a, b] :
+         spblas::__ranges::views::zip(ref_colind, test_colind)) {
+      EXPECT_EQ(a, b);
+    }
   }
 }
