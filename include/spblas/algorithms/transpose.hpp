6917a78f9f02f586a11f93fa2e0b94ff3f5c1a6e
Tobias Ribizel <mail@ribizel.de>
2025-03-06 02:12:55 +0100

    Add transpose algorithm (#34)
    * add transpose algorithm
    
    ---------
    
    Co-authored-by: Benjamin Brock <benjamin.brock@intel.com>

--- include/spblas/algorithms/transpose.hpp
+++ include/spblas/algorithms/transpose.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <spblas/concepts.hpp>
+#include <spblas/detail/operation_info_t.hpp>
 
 namespace spblas {
+
+template <matrix A, matrix B>
+operation_info_t transpose_inspect(A&& a, B&& b);
+
+template <matrix A, matrix B>
+void transpose(operation_info_t& info, A&& a, B&& b);
 
 template <matrix M>
 auto transposed(M&& m);
 
-}
+} // namespace spblas
