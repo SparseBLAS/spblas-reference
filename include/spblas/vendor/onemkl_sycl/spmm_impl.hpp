53adaa606a4bc2f76ab6b70cd9cbe367e2efce18
Benjamin Brock <brock@cs.berkeley.edu>
2025-07-05 11:50:25 -0700

    Support both CPU and GPU tests.

--- include/spblas/vendor/onemkl_sycl/spmm_impl.hpp
+++ include/spblas/vendor/onemkl_sycl/spmm_impl.hpp
@@ -27,7 +27,7 @@
 
 namespace spblas {
 
-template <matrix A, matrix X, matrix Y>
+template <typename ExecutionPolicy, matrix A, matrix X, matrix Y>
   requires(
       (__detail::has_csr_base<A> || __detail::has_csc_base<A>) &&
       __detail::has_mdspan_matrix_base<X> && __detail::is_matrix_mdspan_v<Y> &&
@@ -35,14 +35,15 @@ template <matrix A, matrix X, matrix Y>
                      __mdspan::layout_right> &&
       std::is_same_v<typename std::remove_cvref_t<Y>::layout_type,
                      __mdspan::layout_right>)
-void multiply(A&& a, X&& x, Y&& y) {
+void multiply(ExecutionPolicy&& policy, A&& a, X&& x, Y&& y) {
   log_trace("");
   auto x_base = __detail::get_ultimate_base(x);
 
   auto alpha_optional = __detail::get_scaling_factor(a, x);
   tensor_scalar_t<A> alpha = alpha_optional.value_or(1);
 
-  sycl::queue q(sycl::cpu_selector_v);
+  auto a_data = __detail::get_ultimate_base(a).values().data();
+  auto&& q = __mkl::get_queue(policy, a_data);
 
   auto a_handle = __mkl::get_matrix_handle(q, a);
   auto a_transpose = __mkl::get_transpose(a);
@@ -56,6 +57,19 @@ void multiply(A&& a, X&& x, Y&& y) {
   if (!__detail::has_matrix_opt(a)) {
     oneapi::mkl::sparse::release_matrix_handle(q, &a_handle).wait();
   }
+}
+
+template <matrix A, matrix X, matrix Y>
+  requires(
+      (__detail::has_csr_base<A> || __detail::has_csc_base<A>) &&
+      __detail::has_mdspan_matrix_base<X> && __detail::is_matrix_mdspan_v<Y> &&
+      std::is_same_v<typename __detail::ultimate_base_type_t<X>::layout_type,
+                     __mdspan::layout_right> &&
+      std::is_same_v<typename std::remove_cvref_t<Y>::layout_type,
+                     __mdspan::layout_right>)
+void multiply(A&& a, X&& x, Y&& y) {
+  multiply(mkl::par, std::forward<A>(a), std::forward<X>(x),
+           std::forward<Y>(y));
 }
 
 } // namespace spblas
